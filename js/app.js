bfbf4482ca1a797fe7bfb6d9c4a0e24bb29a0ad6
i3games <straeubig@gmail.com>
2018-02-26 02:12:20 +0000

    user gesture and page reload

--- js/app.js
+++ js/app.js
@@ -7,20 +7,33 @@ var numFolders = 50;
 var scene = document.querySelector('a-scene');
 
 function run() {
+  var playButton = document.getElementById('play-button');
+  var video1 = document.getElementById('video1');
+  var video2 = document.getElementById('video2');
+  var video3 = document.getElementById('video3');
+  var video4 = document.getElementById('video4');
+  var video5 = document.getElementById('video5');
+  var video6 = document.getElementById('video6');
+  playButton.innerText = 'START';
+  playButton.addEventListener('click', function (e) {
+    console.log('button clicked');
+    this.style.display = 'none';
+    video1.play();
+    video2.play();
+    video3.play();
+    video4.play();
+    video5.play();
+    video6.play();
+  }, false);
+
   controller.onFinishChange(function (selected) {
     var folder = selected.substring(1); // workaround for dat.GUI bug sorting strings
-
     localStorage.setItem('folder', folder);
-    document.getElementById('panel1').setAttribute('src', `assets/${folder}/dimension1.mp4`);
-    document.getElementById('panel2').setAttribute('src', `assets/${folder}/dimension2.mp4`);
-    document.getElementById('panel3').setAttribute('src', `assets/${folder}/dimension3.mp4`);
-    document.getElementById('panel4').setAttribute('src', `assets/${folder}/dimension4.mp4`);
-    document.getElementById('panel5').setAttribute('src', `assets/${folder}/dimension5.mp4`);
-    document.getElementById('panel6').setAttribute('src', `assets/${folder}/dimension6.mp4`);
+    location.reload();
   });
 }
 
-for (i = 1; i < numFolders; i++) {
+for (i = 1; i <= numFolders; i++) {
   folders.push(i < 10 ? ' 0' + i : ' ' + i); // the leading space is a workaround for a dat.GUI bug sorting strings
 }
 
@@ -30,5 +43,12 @@ if (folder == null) {
   folder = '01'; // default
 }
 controller.setValue(folder);
+
+document.getElementById('video1').setAttribute('src', `assets/${folder}/dimension1.mp4`);
+document.getElementById('video2').setAttribute('src', `assets/${folder}/dimension2.mp4`);
+document.getElementById('video3').setAttribute('src', `assets/${folder}/dimension3.mp4`);
+document.getElementById('video4').setAttribute('src', `assets/${folder}/dimension4.mp4`);
+document.getElementById('video5').setAttribute('src', `assets/${folder}/dimension5.mp4`);
+document.getElementById('video6').setAttribute('src', `assets/${folder}/dimension6.mp4`);
 
 if (scene.hasLoaded === true) { run(); } else { scene.addEventListener('loaded', run); }
